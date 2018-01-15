262fdef8c7e96309ed22d9d3a8cdf0527e819a2c
ivycsnunez@gmail.com <ivycsnunez@gmail.com>
2018-01-15 05:12:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 } else {
-  console.log("Expected the name to be Susan")
+  console.log("Expected the name to be Joe")
 }
 of (height === "74") {
   console.log("The height is correct")
